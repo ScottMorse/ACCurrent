b7a88f01c0d78421b79abdce3347e6d99c813523
Scott Morse <scottmorsedev@gmail.com>
2019-02-14 09:51:51 -0600

    Update ac.py

--- ac.py
+++ ac.py
@@ -1,7 +1,7 @@
 import numpy as np
 from time import time
 
-def ac(amplitude=9,frequency=60,seconds=2,phases=3,phase_degrees=120):
+def ac(amplitude=110,frequency=60,seconds=2,phases=3,phase_degrees=120):
     
     time_stamp = time()
     second_position = time_stamp / 1000 - int(time_stamp / 1000)
